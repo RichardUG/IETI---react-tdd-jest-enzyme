4acf0b8245e361ed2720967a7c01e6b61c0eb4f1
RichardUG <rsug.rsug@gmail.com>
2022-02-21 21:23:03 -0500

    Fin introducción

---
+++ src/pages/login/index.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react';
+import { Grid,Paper, Avatar, TextField, Button} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import UserIcon from '@mui/icons-material/Person';
+import PasswordIcon from '@mui/icons-material/Password';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import './login.css';
+
+const Login=()=>{
+
+    const paperStyle={padding :20,height:'70vh',width:310, margin:"20px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+
+    const [showPassword] = useState(false);
+
+    const[currentUserData] =useState([])
+
+
+    return(
+        <Grid>
+            <form onSubmit="" className="" >
+                <Paper elevation={10} style={paperStyle} className="fondologin">
+                    <Grid align='center'>
+                        <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                        <h2>Iniciar Sesion</h2>
+                    </Grid>
+                    <div className="text login">
+                        <div>
+                            <ListItemIcon><UserIcon className="icons"/> </ListItemIcon>
+                            <TextField variant="outlined" id="user" name="user" label="Username" type="email"
+                                value={currentUserData?.email} />
+                        </div>
+                        <br></br>
+                        <div >
+                            <ListItemIcon><PasswordIcon className="icons"/> </ListItemIcon>
+                            <FormControl className="" variant="outlined" >
+                                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                                <OutlinedInput fullWidth label="Password"
+                                    id="outlined-adornment-password-login"
+                                    type={showPassword? 'text' : 'password'}
+                                    name="password"
+                                    autoComplete="off"
+                                />
+                            </FormControl>
+                        </div >
+                    </div>
+                    <br/><br/><br/>
+                    <Button className="botonlogin" type='submit' color='primary' variant="contained" fullWidth>Sign in</Button>
+                </Paper>
+            </form>
+        </Grid>
+    )
+}
+
+export default Login;
